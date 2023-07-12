58ceecbdcb2d5fe8ea94e9e9eed163cae665d1ce
Jacob-Roessler <jroess852@gmail.com>
2023-07-11 20:18:35 -0400

    error handling

--- app/api/list/[username]/route.ts
+++ app/api/list/[username]/route.ts
@@ -1,20 +1,24 @@
 import { NextResponse } from 'next/server';
 
 async function fetchData(username: string, url: string, data: object[]) {
-  let req = await fetch(url, {
-    method: 'get',
-    headers: new Headers({
-      Authorization: process.env.MAL_TOKEN!,
-      'Content-Type': 'application/json',
-    }),
-    next: { revalidate: 360 },
-  });
-  let response = await req.json();
-  data = [...data, ...response.data];
-  if (response?.paging?.next) {
-    return fetchData(username, response.paging.next, [...data]);
-  } else {
-    return data;
+  try {
+    let req = await fetch(url, {
+      method: 'get',
+      headers: new Headers({
+        Authorization: process.env.MAL_TOKEN!,
+        'Content-Type': 'application/json',
+      }),
+      next: { revalidate: 360 },
+    });
+    let response = await req.json();
+    data = [...data, ...response.data];
+    if (response?.paging?.next) {
+      return fetchData(username, response.paging.next, [...data]);
+    } else {
+      return data;
+    }
+  } catch (e) {
+    return [];
   }
 }
 
